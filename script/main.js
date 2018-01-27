e468ee4d1e7f1198a9ff83d62b6c41b8cb777c86
yorifuji <yorifuji@gmail.com>
2018-01-27 18:41:10 +0900

    ビデオのレイアウト

--- script/main.js
+++ script/main.js
@@ -6,13 +6,37 @@ var vm = new Vue({
     formatted_user: function(user) {
       return `user.peerId:${user.peerId}`;
     },
+    calc_layout: function() {
+      this.users.forEach((user, index) => {
+        if (index == 0) {
+          user.style = {
+            top: "0px",
+            left: "0px",
+            width: "100%",
+            height: "100%",
+            zIndex: 1,
+          }
+        }
+        else {
+          user.style = {
+            top: "100px",
+            left: 100 * index + "px",
+            width : "160px",
+            height: "120px",
+            zIndex: 2,
+          }
+        }
+      });
+    },
     create_user: function(peerId) {
       return {
         peerId: peerId,
         stream: null,
         style: {
-          top: 0,
-          left: 0,
+          top: "100px",
+          left: "100px",
+          width : "320px",
+          height: "240px",
         }
       }
     },
@@ -20,12 +44,15 @@ var vm = new Vue({
       this.users.forEach(user => {
         if (user.peerId == peerId) user.stream = stream;
       });
+      this.calc_layout();
     },
     join_user: function(peerId) {
       this.users.push(this.create_user(peerId));
+      this.calc_layout();
     },
     leave_user: function(peerId) {
       this.users = this.users.filter(user => user.peerId != peerId);
+      this.calc_layout();
     },
     add_peer: function(peerId, peer) {
       this.peers.push({
