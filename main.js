dd5294db43b387428062409f09ba62e2f9fa69d7
yorifuji <yorifuji@gmail.com>
2018-02-10 17:05:58 +0900

    statsの出力

--- main.js
+++ main.js
@@ -59,9 +59,7 @@ var vm = new Vue({
       else if (this.skyway.conn_type.using.value == "mesh") {
         let options = { mode: 'mesh', stream: this.skyway.stream };
         if (this.video.codec.using) {
-          this.video.codec.mode.forEach(mode => {
-            if (mode.value == this.video.codec.using) options.videoCodec = mode.value;
-          });
+          options.videoCodec = this.video.codec.using.value;
         }
         _dtr(options);
 
@@ -122,6 +120,17 @@ var vm = new Vue({
     select_layout: function (mode) {
       this.layout.using = mode;
       this.calc_layout();
+    },
+    select_stats: function () {
+      if (this.skyway.stats) {
+        clearInterval(window.timer_stats);
+        this.skyway.stats = null;
+      }
+      else {
+        window.timer_stats = setInterval(() => {
+          if (this.skyway.call) getRTCStats(this.skyway.call._negotiator._pc.getStats());
+        }, 2000);
+      }
     },
     calc_layout: function () {
       if (this.layout.using.value == "pinp") {
@@ -416,9 +425,7 @@ var vm = new Vue({
 
       let params = {};
       if (this.video.codec.using) {
-        this.video.codec.mode.forEach(mode => {
-          if (mode.value == this.video.codec.using) params.videoCodec = mode.value;
-        });
+        params.videoCodec = this.video.codec.using.value;
       }
       _dtr(params);
 
@@ -484,6 +491,7 @@ var vm = new Vue({
       room: null,
       stream: null,
       callto: null,
+      stats: "",
     },
     microphone: {
       device: [],
@@ -496,8 +504,8 @@ var vm = new Vue({
     video: {
       codec: {
         mode: [
-          { label: "VP8",  value: "VP8" },
-          { label: "VP9",  value: "VP9" },
+          { label: "VP8",  value: "VP8"  },
+          { label: "VP9",  value: "VP9"  },
           { label: "H264", value: "H264" },
         ],
         using: null,
@@ -541,4 +549,151 @@ var vm = new Vue({
     }
   },
 });
+
+async function getRTCStats(statsObject){
+
+  let trasportArray = [];
+  let candidateArray = [];
+  let candidatePairArray = [];
+  let inboundRTPAudioStreamArray = [];
+  let inboundRTPVideoStreamArray = [];
+  let outboundRTPAudioStreamArray = [];
+  let outboundRTPVideoStreamArray = [];
+  let codecArray = [];
+  let mediaStreamTrack_local_audioArray = [];
+  let mediaStreamTrack_local_videoArray = [];
+  let mediaStreamTrack_remote_audioArray = [];
+  let mediaStreamTrack_remote_videoArray = [];
+  let candidatePairId = '';
+  let localCandidateId = '';
+  let remoteCandidateId = '';
+  let localCandidate = {};
+  let remoteCandidate = {};
+  let inboundAudioCodec = {};
+  let inboundVideoCodec = {};
+  let outboundAudioCode = {};
+  let outboundVideoCode = {};
+
+  let stats = await statsObject;
+  stats.forEach(stat => {
+      if(stat.id.indexOf('RTCTransport') !== -1){
+          trasportArray.push(stat);
+      }
+      if(stat.id.indexOf('RTCIceCandidatePair') !== -1){
+          candidatePairArray.push(stat);
+      }
+      if(stat.id.indexOf('RTCIceCandidate_') !== -1){
+          candidateArray.push(stat);
+      }
+      if(stat.id.indexOf('RTCInboundRTPAudioStream') !== -1){
+          inboundRTPAudioStreamArray.push(stat);
+      }
+      if(stat.id.indexOf('RTCInboundRTPVideoStream') !== -1){
+          inboundRTPVideoStreamArray.push(stat);
+      }
+      if(stat.id.indexOf('RTCOutboundRTPAudioStream') !== -1){
+          outboundRTPAudioStreamArray.push(stat);
+      }
+      if(stat.id.indexOf('RTCOutboundRTPVideoStream') !== -1){
+          outboundRTPVideoStreamArray.push(stat);
+      }
+      if(stat.id.indexOf('RTCMediaStreamTrack_local_audio') !== -1){
+          mediaStreamTrack_local_audioArray.push(stat);
+      }
+      if(stat.id.indexOf('RTCMediaStreamTrack_local_video') !== -1){
+          mediaStreamTrack_local_videoArray.push(stat);
+      }
+      if(stat.id.indexOf('RTCMediaStreamTrack_remote_audio') !== -1){
+          mediaStreamTrack_remote_audioArray.push(stat);
+      }
+      if(stat.id.indexOf('RTCMediaStreamTrack_remote_video') !== -1){
+          mediaStreamTrack_remote_videoArray.push(stat);
+      }
+      if(stat.id.indexOf('RTCCodec') !== -1){
+          codecArray.push(stat);
+      }
+  });
+
+  trasportArray.forEach(transport => {
+      if(transport.dtlsState === 'connected'){
+          candidatePairId = transport.selectedCandidatePairId;
+      }
+  });
+  candidatePairArray.forEach(candidatePair => {
+      if(candidatePair.state === 'succeeded' && candidatePair.id === candidatePairId){
+          localCandidateId = candidatePair.localCandidateId;
+          remoteCandidateId = candidatePair.remoteCandidateId;
+      }
+  });
+  candidateArray.forEach(candidate => {
+      if(candidate.id === localCandidateId){
+          localCandidate = candidate;
+      }
+      if(candidate.id === remoteCandidateId){
+          remoteCandidate = candidate;
+      }
+  });
+
+  inboundRTPAudioStreamArray.forEach(inboundRTPAudioStream => {
+      codecArray.forEach(codec => {
+          if(inboundRTPAudioStream.codecId === codec.id){
+              inboundAudioCodec = codec;
+          }
+      });
+  });
+  inboundRTPVideoStreamArray.forEach(inboundRTPVideoStream => {
+      codecArray.forEach(codec => {
+          if(inboundRTPVideoStream.codecId === codec.id){
+              inboundVideoCodec = codec;
+          }
+      });
+  });
+  outboundRTPAudioStreamArray.forEach(outboundRTPAudioStream => {
+      codecArray.forEach(codec => {
+          if(outboundRTPAudioStream.codecId === codec.id){
+              outboundAudioCodec = codec;
+          }
+      });
+  });
+  outboundRTPVideoStreamArray.forEach(outboundRTPVideo => {
+      codecArray.forEach(codec => {
+          if(outboundRTPVideo.codecId === codec.id){
+              outboundVideoCodec = codec;
+          }
+      });
+  });
+
+  let text = "";
+  text += "Local Candidate\n";
+  text += localCandidate.ip + ':' + localCandidate.port + '(' +localCandidate.protocol + ')' + '\ntype:' + localCandidate.candidateType;
+  text += '\n'
+  text += "Remote Candidate\n";
+  text += remoteCandidate.ip + ':' + remoteCandidate.port + '(' +remoteCandidate.protocol + ')' + '\ntype:' + remoteCandidate.candidateType;
+  text += '\n'
+  text += "Inbound Codec\n";
+  text += inboundVideoCodec.mimeType + '\n' + inboundAudioCodec.mimeType;
+  text += '\n'
+  text += "Outbound Codec\n";
+  text += outboundVideoCodec.mimeType + '\n' + outboundAudioCodec.mimeType;
+  text += '\n'
+  text += "Inbound Audio\n";
+  text += 'bytesReceived:' + inboundRTPAudioStreamArray[0].bytesReceived + '\njitter:' + inboundRTPAudioStreamArray[0].jitter + '\nfractionLost:' + inboundRTPAudioStreamArray[0].fractionLost;
+  text += '\n'
+  text += "Inbound Video\n";
+  text += 'bytesReceived:' + inboundRTPVideoStreamArray[0].bytesReceived + '\nfractionLost:' + inboundRTPVideoStreamArray[0].fractionLost;
+  text += '\n'
+  text += "Outbound Audio\n";
+  text += 'bytesReceived:' + outboundRTPAudioStreamArray[0].bytesSent;
+  text += '\n'
+  text += "Outbound Video\n";
+  text += 'bytesReceived:' + outboundRTPVideoStreamArray[0].bytesSent;
+  text += '\n'
+  text += "Local Audio/Vidoe Track\n";
+  text += 'audioLevel:' + mediaStreamTrack_local_audioArray[0].audioLevel + '\nframeHeight:' + mediaStreamTrack_local_videoArray[0].frameHeight + '\nframeWidth:' + mediaStreamTrack_local_videoArray[0].frameWidth + '\nframesSent:' + mediaStreamTrack_local_videoArray[0].framesSent;
+  text += '\n'
+  text += "Remote Audio/Video Track\n";
+  text += 'audioLevel:' + mediaStreamTrack_remote_audioArray[0].audioLevel + '\nframeHeight:' + mediaStreamTrack_remote_videoArray[0].frameHeight + '\nframeWidth:' + mediaStreamTrack_remote_videoArray[0].frameWidth;
+  vm.skyway.stats = text;
+  console.log(text);
+}
 
